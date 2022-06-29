09411df0ff4415d792675a3564b7945bda7a9e1c
natchapon <natchapon.boontharaksa@gmail.com>
2022-06-29 23:28:20 +0700

    [Add] - Modify schema of smartpole

--- mongo/models/smartPole.js
+++ mongo/models/smartPole.js
@@ -1,5 +1,36 @@
 const mongoose = require('mongoose');
 
+/*
+{
+    _id,
+    Token: String,
+    date: {
+        day: Number,
+        month: Number,
+        year: Number
+    },
+    time: {
+        hour: Number,
+        min: Number,
+        sec: Number
+    },
+    deviceInfo: {
+        deviceId: String,
+        deviceStatus: Boolean,
+        deviceSignal: String,
+    },
+    sensorInfo: {
+        powerConsumption: Number,
+        batteryVoltage: Number,
+        batteryCurrent: Number,
+        solarVoltage: Number,
+        solarCurrent: Number,
+        lightValue: Number,
+        location: [Number, Number]
+    }
+}
+*/
+
 const smartPoleSchema = mongoose.Schema({
     id: String,
     deviceStatus: Boolean
